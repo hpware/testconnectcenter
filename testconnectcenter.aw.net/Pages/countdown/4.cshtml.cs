fc51253475fb61fbd6fd8eff7a7f494cb949a27b
howar <howar@DESKTOP-KCFPOTQ>
2024-01-28 14:35:30 +0800

    Add project files.

---
+++ testconnectcenter.aw.net/Pages/countdown/4.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testconnectcenter.Pages.countdown
+{
+    public class _4Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
