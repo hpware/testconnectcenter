836b1b65f5bb0154e6d0ef14b04a93032562e8d7
ictechz <howard9123@outlook.com>
2024-02-06 14:43:05 +0800

    Add annoucements (test)

---
+++ testconnectcenter.aw.net/Pages/announcement/2024-2-6-5-55.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testconnectcenter.Pages.announcement
+{
+    public class _2024_2_6_5_55Model : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ testconnectcenter.aw.net/Pages/announcement/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testconnectcenter.Pages.announcement
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
