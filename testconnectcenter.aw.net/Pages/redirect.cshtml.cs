e2745b2c231c20c57bd54735f8cb35e67722cb24
ictechz <howard9123@outlook.com>
2024-02-18 22:27:27 +0800

    加 Redirect 的地方 Redirect.cshtml 和 _Layout.cshtml

---
+++ testconnectcenter.aw.net/Pages/redirect.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testconnectcenter.Pages
+{
+    public class redirectModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
