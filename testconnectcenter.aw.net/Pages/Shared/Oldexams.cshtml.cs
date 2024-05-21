4ab0300fb831746700bbb620def3f76b8b2b6f38
ictechz <howard9123@outlook.com>
2024-05-21 20:13:05 +0800

    Change the favicon.ico & add 113exam & oldexams tab & shared include Oldexams.cshtml as a share tab across oldexams tab.

---
+++ testconnectcenter.aw.net/Pages/Shared/Oldexams.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace testconnectcenter.Pages.Shared
+{
+    public class OldexamsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
